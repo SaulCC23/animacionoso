8182400d36e8e56ffbb1ad6fbc92eedd1ba3093d
SaulCC23 <cetinacetinasaul@gmail.com>
2025-09-04 17:02:46 -0600

    feat (rive): animation added

---
+++ android/app/src/main/kotlin/com/example/animacionoso/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animacionoso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key, required String title});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: RiveAnimation.asset('animated_login_character.riv')),
+        ],
+      ),
+    );
+  }
+} 
